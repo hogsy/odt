9c9d32b73f3621554e26968ea80a906cee1c4d8c
hogsy <markelswo@gmail.com>
2021-05-03 17:12:16 +0100

    GroupFile loader.

---
+++ Baron/GroupFile.cpp
@@ -0,0 +1,89 @@
+/*
+MIT License
+
+Copyright (c) 2021 Mark Sowden
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <cstdio>
+#include <cstring>
+
+#include "GroupFile.h"
+
+using namespace baron;
+
+GroupFile::GroupFile(uint8_t *nBuffer, unsigned int nLength, unsigned int *errCode) :
+        buffer(nBuffer), length(nLength) {
+    *errCode = SetupIndexTable();
+}
+
+GroupFile::GroupFile(const char *path, unsigned int *errCode) {
+    FILE *file = fopen(path, "rb");
+    if (file == nullptr) {
+        *errCode = GF_ERR_IO;
+        return;
+    }
+
+    fseek(file, 0, SEEK_END);
+    length = ftell(file);
+    rewind(file);
+    if(length <= 0) {
+        *errCode = GF_ERR_IO;
+    } else {
+        buffer = new uint8_t[length];
+        fread(buffer, sizeof(uint8_t), length, file);
+        *errCode = SetupIndexTable();
+        if( *errCode != GF_ERR_SUCCESS ) {
+            delete buffer;
+        }
+    }
+
+    fclose(file);
+}
+
+GroupFile::~GroupFile() {
+    delete buffer;
+}
+
+void *GroupFile::ReadIndex(unsigned int index, int *outLength) {
+    if (index >= indices.size()) {
+        return nullptr;
+    }
+
+    uint8_t *offsetPtr = buffer + indices[index].offset;
+
+    uint8_t *rb = new uint8_t[indices[index].length];
+    memcpy(rb, offsetPtr, indices[index].length);
+    *outLength = indices[index].length;
+
+    return rb;
+}
+
+unsigned int GroupFile::SetupIndexTable() {
+    int numIndices = *buffer;
+    if (numIndices <= 0) {
+        return GF_ERR_EMPTY_FILE;
+    }
+
+    indices.resize(numIndices);
+    memcpy(indices.data(), buffer + 4, sizeof(Index) * numIndices);
+
+    return GF_ERR_SUCCESS;
+}
---
+++ Baron/GroupFile.h
@@ -0,0 +1,62 @@
+/*
+MIT License
+
+Copyright (c) 2021 Mark Sowden
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace baron {
+    class GroupFile {
+    public:
+        enum {
+            GF_ERR_SUCCESS,
+            GF_ERR_IO,
+            GF_ERR_EMPTY_FILE,
+        };
+
+        explicit GroupFile(const char *path, unsigned int *errCode);
+        GroupFile(uint8_t *nBuffer, unsigned int nLength, unsigned int *errCode);
+        ~GroupFile();
+
+        inline unsigned int GetNumberOfIndices() const {
+            return indices.size();
+        }
+
+        void *ReadIndex(unsigned int index, int *outLength);
+
+    protected:
+    private:
+        unsigned int SetupIndexTable();
+
+        struct Index {
+            int offset;
+            int length;
+        };
+        std::vector<Index> indices;
+
+        uint8_t *buffer{ nullptr };
+        unsigned int length{ 0 };
+    };
+}
